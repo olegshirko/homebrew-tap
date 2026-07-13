2ef56f897f37813890ef4b74edbb7f90e8ea2fb1
oleg <oleg@Mac.lan>
2026-07-13 23:30:16 +0300

    v1.0.8

--- anvil.rb
+++ anvil.rb
@@ -1,12 +1,12 @@
 class Anvil < Formula
   desc "Lightweight macOS Docker environment using Virtualization.framework"
   homepage "https://github.com/olegshirko/anvil"
-  version "1.0.7"
+  version "1.0.8"
   license "Apache-2.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/olegshirko/anvil/releases/download/v1.0.7/vz-runner-darwin-arm64"
-    sha256 "5ba51e74e43a5c84c56d43983b4d6b19908fe7e6140ec02a73f4a46181646513"
+    url "https://github.com/olegshirko/anvil/releases/download/v1.0.8/vz-runner-darwin-arm64"
+    sha256 "91eeb8fd1c0e861316eb01b48e6ea51fdb1c948d7f8e6f4712fce8923065da8f"
   end
 
   def install
