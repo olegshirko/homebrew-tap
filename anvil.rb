c660d9c989afadfbe498dd2fa17dfd859db0f14a
oleg <oleg@MacBook-Pro-von-oleg.local>
2026-07-14 10:43:37 +0300

    fix: move url/sha256 to top level for zerobrew compatibility

--- anvil.rb
+++ anvil.rb
@@ -3,11 +3,8 @@ class Anvil < Formula
   homepage "https://github.com/olegshirko/anvil"
   version "1.0.8"
   license "Apache-2.0"
-
-  if Hardware::CPU.arm?
-    url "https://github.com/olegshirko/anvil/releases/download/v1.0.8/vz-runner-darwin-arm64"
-    sha256 "91eeb8fd1c0e861316eb01b48e6ea51fdb1c948d7f8e6f4712fce8923065da8f"
-  end
+  url "https://github.com/olegshirko/anvil/releases/download/v1.0.8/vz-runner-darwin-arm64"
+  sha256 "91eeb8fd1c0e861316eb01b48e6ea51fdb1c948d7f8e6f4712fce8923065da8f"
 
   def install
     bin.install "vz-runner-darwin-arm64" => "anvil"
