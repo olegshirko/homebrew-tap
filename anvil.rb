0ec6d8cf3e40710e53cb421b320c4fddda469c1d
oleg <oleg@MacBook-Pro-von-oleg.local>
2026-07-14 11:03:50 +0300

    v1.0.10

--- anvil.rb
+++ anvil.rb
@@ -1,10 +1,10 @@
 class Anvil < Formula
   desc "Lightweight macOS Docker environment using Virtualization.framework"
   homepage "https://github.com/olegshirko/anvil"
-  version "1.0.9"
+  version "1.0.10"
   license "Apache-2.0"
-  url "https://github.com/olegshirko/anvil/releases/download/v1.0.9/anvil-darwin-arm64.tar.gz"
-  sha256 "bb12f1fcec59ac7c8bef24a4dc06d65f54e80b119e2e7c38cca2f37a03cf043a"
+  url "https://github.com/olegshirko/anvil/releases/download/v1.0.10/anvil-darwin-arm64.tar.gz"
+  sha256 "80dee8529712bbe662f75f0b4eb4209c14920abfa374dae52e83875ec7a3b613"
 
   def install
     bin.install "vz-runner-darwin-arm64" => "anvil"
