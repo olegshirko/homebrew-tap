705296e517eb0d5de614f08c49f75ac6e77e1e11
oleg <oleg@MacBook-Pro-von-oleg.local>
2026-07-14 13:48:06 +0300

    v1.0.15

--- anvil.rb
+++ anvil.rb
@@ -4,7 +4,7 @@ class Anvil < Formula
   version "1.0.15"
   license "Apache-2.0"
   url "https://github.com/olegshirko/anvil/releases/download/v1.0.15/anvil-darwin-arm64.tar.gz"
-  sha256 "2bc548bef3f9f0eb0bef5873e90803e9db207a40a620adec9425c0be6020b89b"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   resource "vmlinuz" do
     url "https://github.com/olegshirko/anvil/releases/download/v1.0.15/vmlinuz-raw"
