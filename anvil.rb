c06d58095d6972b2323e13d94b00bf90f751445e
oleg <oleg@MacBook-Pro-von-oleg.local>
2026-07-14 13:36:27 +0300

    v1.0.13

--- anvil.rb
+++ anvil.rb
@@ -1,13 +1,30 @@
 class Anvil < Formula
   desc "Lightweight macOS Docker environment using Virtualization.framework"
   homepage "https://github.com/olegshirko/anvil"
-  version "1.0.14"
+  version "1.0.13"
   license "Apache-2.0"
-  url "https://github.com/olegshirko/anvil/releases/download/v1.0.14/anvil-darwin-arm64.tar.gz"
-  sha256 "dcbf57f72838a1d31ac79d64558a7818038ffcb1787a709314ad0d234b9d82d8"
+  url "https://github.com/olegshirko/anvil/releases/download/v1.0.13/anvil-darwin-arm64.tar.gz"
+  sha256 "5029cf709af2cb3062dd89c6aaf1acddae53427c2a7fbc04c89ff29edd05b172"
+
+  resource "vmlinuz" do
+    url "https://github.com/olegshirko/anvil/releases/download/v1.0.13/vmlinuz-raw"
+  end
+
+  resource "initramfs" do
+    url "https://github.com/olegshirko/anvil/releases/download/v1.0.13/initramfs-containerd"
+  end
 
   def install
     bin.install "vz-runner-darwin-arm64" => "anvil"
+    state_dir = "#{ENV["HOME"]}/.anvil-vz"
+    resource("vmlinuz").stage do
+      FileUtils.mkdir_p(state_dir)
+      FileUtils.cp "vmlinuz-raw", state_dir
+    end
+    resource("initramfs").stage do
+      FileUtils.mkdir_p(state_dir)
+      FileUtils.cp "initramfs-containerd", state_dir
+    end
   end
 
   test do
