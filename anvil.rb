605ec713e94dffb8d327262b9220dfba8528f238
oleg <oleg@MacBook-Pro-von-oleg.local>
2026-07-14 11:01:05 +0300

    fix: use tar.gz for zerobrew compatibility

--- anvil.rb
+++ anvil.rb
@@ -3,8 +3,8 @@ class Anvil < Formula
   homepage "https://github.com/olegshirko/anvil"
   version "1.0.9"
   license "Apache-2.0"
-  url "https://github.com/olegshirko/anvil/releases/download/v1.0.9/vz-runner-darwin-arm64"
-  sha256 "73473a86383361cabcb00cec2c32598f6b7a753d428e53c36b2c8f552a2208eb"
+  url "https://github.com/olegshirko/anvil/releases/download/v1.0.9/anvil-darwin-arm64.tar.gz"
+  sha256 "bb12f1fcec59ac7c8bef24a4dc06d65f54e80b119e2e7c38cca2f37a03cf043a"
 
   def install
     bin.install "vz-runner-darwin-arm64" => "anvil"
