bbf4a6115948b3a70135df4a8c0e8997c9be27f2
Oleg Shirko <oleg.shirko@gmail.com>
2026-07-15 10:49:23 +0300

    v1.0.32

--- anvil.rb
+++ anvil.rb
@@ -1,8 +1,8 @@
 class Anvil < Formula
   desc "Lightweight macOS Docker environment using Virtualization.framework"
   homepage "https://github.com/olegshirko/anvil"
-  url "https://github.com/olegshirko/anvil/releases/download/v1.0.31/anvil-darwin-arm64.tar.gz"
-  sha256 "f842530902e261b435caf5556deabaa83e512b3e58d4f71f23712810dec3298b"
+  url "https://github.com/olegshirko/anvil/releases/download/v1.0.32/anvil-darwin-arm64.tar.gz"
+  sha256 "dc384eb0c5d1c34d46ec6ba132cea9b6eda7d659440ee44faa65d6155d34e95e"
   license "Apache-2.0"
 
   depends_on arch: :arm64
