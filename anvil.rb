2f78b3a2399f5f52d3f9bdc71960241fc0036fa6
Oleg Shirko <oleg.shirko@gmail.com>
2026-07-15 10:50:16 +0300

    fix: explicit version in formula

--- anvil.rb
+++ anvil.rb
@@ -2,6 +2,7 @@ class Anvil < Formula
   desc "Lightweight macOS Docker environment using Virtualization.framework"
   homepage "https://github.com/olegshirko/anvil"
   url "https://github.com/olegshirko/anvil/releases/download/v1.0.32/anvil-darwin-arm64.tar.gz"
+  version "1.0.32"
   sha256 "dc384eb0c5d1c34d46ec6ba132cea9b6eda7d659440ee44faa65d6155d34e95e"
   license "Apache-2.0"
 
