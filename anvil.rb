1292d284e75c9c894a058e1e7a22f1e43d77b39e
Oleg Shirko <oleg.shirko@gmail.com>
2026-08-12 11:44:10 +0300

    v1.0.45

--- anvil.rb
+++ anvil.rb
@@ -1,20 +1,14 @@
 class Anvil < Formula
   desc "Lightweight macOS Docker environment using Virtualization.framework"
   homepage "https://github.com/olegshirko/anvil"
-  url "https://github.com/olegshirko/anvil/releases/download/v1.0.44/anvil-darwin-arm64.tar.gz"
-  version "1.0.44"
-  sha256 "af63d2731bdc3e97c8cfb64b07ad65daa6ae480f1aa2418aca163b0337051512"
+  url "https://github.com/olegshirko/anvil/releases/download/v1.0.45/anvil-darwin-arm64.tar.gz"
+  version "1.0.45"
+  sha256 "f27bbca1101f52f47a6d92a1979df0871ed63850e48f0faa25f63b480480e09b"
   license "Apache-2.0"
 
   depends_on arch: :arm64
   depends_on :macos
 
-  bottle do
-    root_url "https://github.com/olegshirko/anvil/releases/download/v1.0.44"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "9f6ce1d97df5b92fbc8d284f90841d595be2e3b7ae9a9b1a91d9e2ec8e1e8bfb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9f6ce1d97df5b92fbc8d284f90841d595be2e3b7ae9a9b1a91d9e2ec8e1e8bfb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "9f6ce1d97df5b92fbc8d284f90841d595be2e3b7ae9a9b1a91d9e2ec8e1e8bfb"
-  end
 
 
 
